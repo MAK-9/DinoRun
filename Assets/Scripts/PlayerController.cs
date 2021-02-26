9a952178a7ce49cc5446f95d957009f056b8ee6b
MAK-9 <13michal9@gmail.com>
2021-02-26 19:41:47 +0100

    add death animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,8 +9,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 100f;
     private float horizontalMove=0f;
     public float jumpStrength = 10f;
+
+    private bool dead = false;
+    
     private Rigidbody2D rb;
     private CircleCollider2D collider2D;
+    private Animator animator;
     
     
     // Start is called before the first frame update
@@ -18,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         collider2D = gameObject.GetComponent<CircleCollider2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,16 +31,16 @@ public class PlayerController : MonoBehaviour
         horizontalMove = Input.GetAxisRaw("Horizontal");
         
         //jump
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() && !dead)
         {
-            float verticalMove = jumpStrength;
-            rb.velocity = Vector2.up * verticalMove;
+            Jump();
         }
     }
 
     private void FixedUpdate()
     {
-        Move();
+        if(!dead)
+            Move();
     }
 
     void Move()
@@ -66,9 +71,24 @@ public class PlayerController : MonoBehaviour
         return raycastHit.collider != null;
     }
 
+    void Jump()
+    {
+        float verticalMove = jumpStrength;
+        rb.velocity = Vector2.up * verticalMove;
+    }
+
     public void Die()
     {
-        Debug.Log("Game Over!");
-        //Time.timeScale = 0f;
+        if (!dead)
+        {
+            Debug.Log("Game Over!");
+            //Time.timeScale = 0f;
+
+            dead = true;
+            animator.SetTrigger("Hurt");
+            Jump();
+            animator.SetTrigger("Dead");
+        }
+        
     }
 }
